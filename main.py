12250769b92e9e60729e847053e3f959816457cf
VK <mr.kuznetsov@outlook.com>
2022-02-03 01:28:42 +0300

    test

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
+from urllib import response
 import openpyxl
+import requests
 import datetime
+import json
 
-wb = openpyxl.Workbook()
-
-ws = wb.active
-ws.title = "Price List"
-
-# Header
-
-ws.merge_cells('A2:G2')
-ws['A2'] = 'PRICE LIST'
-
-ws['A5'] = 'Company name'
-ws['A6'] = 'house_street'
-ws['A7'] = 'city_zip'
-
-ws['C5'] = 'Phone'
-ws['C6'] = 'E-mail'
-ws['C7'] = 'Website'
-
-ws['G5'] = 'Valid from'
-ws['G6'] = datetime.date.today()
-
-# Spreadsheet
-
-ws.merge_cells('A9:A10')
-ws.merge_cells('B9:B10')
-ws.merge_cells('C9:C10')
-ws.merge_cells('D9:D10')
-ws.merge_cells('E9:E10')
-ws.merge_cells('F9:F10')
-ws.merge_cells('G9:G10')
-
-
-wb.save(filename='Price_list.xlsx')
+with open('test_data.json') as file:
+    data = json.load(file)
+print(data)
